8fe69d0150085dff101d4149a6f7f7647e88d29c
renatotn7 <renatotn7>
2021-06-16 23:05:11 -0300

    comitando as alterações

---
+++ loadComCluster.py
@@ -0,0 +1,129 @@
+import transformers
+from transformers import pipeline
+import re
+from wordcloud import STOPWORDS, WordCloud
+from argparse import ArgumentParser, Namespace
+from decimal import Decimal
+import requests
+
+import matplotlib.pyplot as plt
+resultados=""
+model_name = 'pierreguillou/bert-base-cased-squad-v1.1-portuguese'
+nlp = pipeline("question-answering", model=model_name)
+parser = ArgumentParser("Squad portugues")
+parser.add_argument("--questao",
+                        required=True,
+                        help="Digite aqui a pergunta.")
+parser.add_argument("--precisao",
+                        required=True,
+                        help="digite aqui a precisão de tolerância, algo como 0.5 ou 0.7 algo assim.")
+                        
+parser.add_argument("--versao",
+                        required=True,
+                        help="por enquanto acf ou nvi.")
+args = parser.parse_args()
+versao=args.versao 
+textointeiro=""
+STOPWORDS = ['ver','principal','essa','vez','nas','mas','qual','principal','ele','ter','doença','pois','este','vez','ver principal','artigo principal','já','aos','pode','outro','artigo','desse','alguns','meio','entre','das','podem','esse','seu','também','são','quando','de', 'que','em','os','as','da','como','dos','ou','se','um','uma','para','na','ao','mais','por','não','ainda','muito','sua'] + list(STOPWORDS)
+def parseHbibonlineHtml2txt(siglivro,cap,de,ate):
+    if len(ate)>0:
+        ate="-"+ate
+    #print("https://www.bibliaonline.com.br/"+versao+"/"+siglivro+"/"+cap+"/"+de+ate)
+    
+    response = requests.post("https://www.bibliaonline.com.br/"+versao+"/"+siglivro+"/"+cap+"/"+de+ate)#,headers=my_headers,json=my_body)
+    conteudo = response.text.split("<div class=\"jss32 jss27\">")[1].split("<div class=\"jss39 jss28\">")[0]
+    conteudo=re.sub('<[^>]+?>', '', conteudo)
+    conteudo=re.sub('\&(lt|gt|quot)\;', '', conteudo)
+    
+    conteudo=re.sub(r'\d', '', conteudo)
+    return conteudo
+
+def resposta(questao, conteudo,identificador):
+    percentual =Decimal(args.precisao)
+    result = nlp(question=questao, context=conteudo)
+    if round(result['score'], 4)>percentual:
+        posfinalstart = result['start']-100
+        posfinalfim=result['end']+100
+        conteudo2 = re.sub(r'\n','',conteudo[posfinalstart:posfinalfim])
+        print(f"{identificador}: \t\t-->*'{result['answer']}'*\t\t, score: {round(result['score'], 4)}, start: {result['start']}, end: {result['end']} \n\t(...{conteudo2}...)\n")
+        return [result['answer'].strip() , round(result['score']*10)];
+    else:
+        return "",0
+
+
+def extraicapitulo(coluna):
+    valor=coluna.split(":")[0]
+    return valor.strip()
+def extraiverp1(coluna):
+    valor=coluna.split(":")[1].split("-")[0]
+    return valor.strip()
+def extraiverp2(coluna):
+    valor=coluna.split(":")[1]
+    parte = valor.split("-")
+    if len(parte)>1:
+        valor= parte[1]
+    else:
+        valor=""
+   
+    return valor.strip()
+f = open('livrosbiblia.txt')
+texto = f.read()
+linhas = texto.split("\n")
+f.close()
+for i in range(1,len(linhas)): #30):
+    
+    content=""
+    linha = linhas[i]
+    coluna = linha.split(";")
+    try:
+        f = open("biblia"+"/"+versao+"/"+coluna[0] + "-" + re.sub('[^\w\s]', '', coluna[1]), "r")
+        content= f.read()
+        conteudo = re.sub(r'’|“|‘|”','',content)
+        conteudo = re.sub(r'\d','',conteudo)
+        conteudo = re.sub(r'\n\.','\n',conteudo)
+        conteudo = re.sub(r'\n ','\n',conteudo)
+        conteudo = re.sub(r'\r\n','\n',conteudo)
+        content = re.sub(r'\n\n','\n',conteudo)
+        f.close()
+    except:
+    
+        if len(coluna[3]) !=0:
+            content = content+"\n"+parseHbibonlineHtml2txt("mt",extraicapitulo(coluna[3]),extraiverp1(coluna[3]),extraiverp2(coluna[3]))
+        if len(coluna[4]) !=0:
+            content = content+"\n"+parseHbibonlineHtml2txt("mc",extraicapitulo(coluna[4]),extraiverp1(coluna[4]),extraiverp2(coluna[4]))
+        if len(coluna[5]) !=0:
+            content =content+"\n"+parseHbibonlineHtml2txt("lc",extraicapitulo(coluna[5]),extraiverp1(coluna[5]),extraiverp2(coluna[5]))
+        if len(coluna[6]) !=0:
+            content =content+"\n"+parseHbibonlineHtml2txt("jo",extraicapitulo(coluna[6]),extraiverp1(coluna[6]),extraiverp2(coluna[6]))     
+        print(coluna[0] + " - " + coluna[1])
+        f = open("evangCruzados"+"/"+versao+"/"+coluna[0] + "-" + re.sub('[^\w\s]', '', coluna[1]), "w")
+        f.write(content)
+        f.close()
+   
+     
+    
+  
+    
+    question = args.questao 
+    #try:
+    #retorno, score = resposta(question,content,coluna[0] + " - " + coluna[1])
+    retorno = "renato"
+    score =0
+    #print('*'+str(score))
+    for j in range(0,score):
+        retorno= retorno + " "+ retorno
+    
+    resultados = resultados +" "+ retorno
+    textointeiro=textointeiro + "\n"+content
+    #except:
+     #   print("An exception occurred")
+    #print(resultados)
+wc = WordCloud(stopwords=STOPWORDS,background_color="white", max_words=2000,
+               max_font_size=256,
+               random_state=42, width=1550, height=1080,collocations=False)
+wc.generate(resultados) #ajuste do tamanho das palavras
+plt.imshow(wc, interpolation="bilinear")
+plt.axis('off')
+print('teste')
+#plt.show()
+retorno, score = resposta(question,textointeiro,"teste")
